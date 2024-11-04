a59859598beff7e430a52713882210efe6272c18
Ana Julia do Nascimento Barros <159742304+JuliaNascimentos@users.noreply.github.com>
2024-11-04 11:25:26 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
+import Profile from "./components/Profile"
+
 export default function App(){
   return(
-    <div>
-        <img src="../../public/react.png" alt="" />
-
-        <h1>React</h1>
-        <p>A biblioteca para interface de usuário web e nativa.</p>
-        <button>Aprenda React</button>
-        <button>Referência da API</button>
-        <hr />
-        <h1>Crie interfaces de usuário de componentes</h1>
-        <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes.</p>
-        <hr />
-        <h1>Escreva componentes com código e marcação</h1>
-        <p>Componentes react são funções JavaScript.A sintaxe de marcação é chamado de JSX. É uma extensão de sintaxe JavaScript popularizada pelo React.</p>
-        <h1>Próximos passos</h1>
-        <ul>
-          <li>Uso de dados dinâmicos no JSX</li>
-          <li>Criação de novos componentes</li>
-          <li>Estilização de componentes</li>
-          <li>Reutilização de componentes</li>
-          <li>Uso de props e children</li>
-          <li>Uso de eventos do JavaScript</li>
-        </ul>
-    </div>
+    <body>
+      <Profile img={'./perfil.png'} name={'Ana Julia do Nascimento Barros'} bio={'Estudante de Desenvolvimento de Sistemas na EEEP Deputado Roberto Mesquita'} phone={'+55 85 9126-2101'} email={'annajulyabarross36@gmail.com'} twitterUrl={'#'} githubUrl={'https://github.com/settings/profile'} linkedinUrl={'https://www.linkedin.com/in/ana-julia-barros-b2ba13283/'}></Profile>
+    </body> 
   )
 }
